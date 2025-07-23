66e0fac7235398e7bf586790b26cc839feac572c
TiSoi07 <hieumonday1707@gmail.com>
2025-07-23 15:04:47 +0700

    Hiếu

---
+++ frontend/pages/about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Myshop</title>
+<?php
+include_once(__DIR__. '/layouts/styles.php')
+?>
+
+
+</head>
+<body>
+   
+    <?php include_once(__DIR__ . '/layouts/partials/header.php'); ?>
+
+
+
+    <?php include_once(__DIR__ . '/layouts/partials/footer.php'); ?> 
+    <?php include_once(__DIR__ . '/layouts/scripts.php'); ?>
+</body>
+</html>
